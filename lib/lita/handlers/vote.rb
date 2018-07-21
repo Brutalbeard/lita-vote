edaba9b09519e6fb8b0626e2a2301e95edb238b3
Brutalbeard <jceloria@gmail.com>
2018-07-21 17:42:59 -0400

    almost... there

--- lib/lita/handlers/vote.rb
+++ lib/lita/handlers/vote.rb
@@ -10,7 +10,11 @@ module Lita
       def initialize(robot)
         super
         
-        @db = Mongo::Client.new("mongodb://#{config.mongodb_user}:#{config.mongodb_password}@#{config.mongodb_address}/#{config.mongodb_database}")
+        @db = Mongo::Client.new([ config.db_address ], 
+          :database => config.mongodb_database,
+          :user => config.mongodb_user,
+          :password => config.mongodb_password
+          )
         @collection = @db[:polls]
       end
 
